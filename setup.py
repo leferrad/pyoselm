79e14b7d84f88e52e8cb830f788aa33168f7f958
leferrad <ljferrado@gmail.com>
2018-03-19 23:51:04 -0300

    Minor update on setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,8 @@ setup(
     url="https://github.com/leferrad/pyoselm",
     packages=find_packages(exclude=['examples', 'test']),
     license="LICENSE",
-    description="",
+    description="A Python implementation of Online Sequential Extreme Machine Learning (OS-ELM) "
+                "for online machine learning",
     long_description=open("README.md").read(),
     install_requires=open("requirements.txt").read().split()
 )
