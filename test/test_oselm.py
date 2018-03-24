c4929d507571b7d5f45243bf3302497a44fc1d7a
leferrad <ljferrado@gmail.com>
2018-03-24 15:53:24 -0300

    Updating tests

--- test/test_oselm.py
+++ test/test_oselm.py
@@ -6,118 +6,103 @@ from __future__ import print_function
 from pyoselm.core import OSELMClassifier, OSELMRegressor
 
 from sklearn.datasets import load_digits, make_regression
-from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import confusion_matrix
 
 import numpy as np
-import matplotlib.pyplot as plt
 import time
 import random
 import sys
 
 
-def make_toy_data(n=20, start=0.25, delta=0.1):
-    end = start + n*delta
-    x = np.arange(start, end, delta)
-    y = x*np.cos(x)+0.5*np.sqrt(x)*np.random.randn(x.shape[0])
-    x = x.reshape(-1, 1)
-    y = y.reshape(-1, 1)
-    return x, y
+def make_batch_predictions(model, x, y, n_batch=50):
+    n_samples = len(y)
+    n_batches = int(n_samples / n_batch)
 
+    print("Dataset has %i samples" % n_samples)
+    print("Testing over %i non-overlapped batches with a max of %i samples..." % (n_batches, n_batch))
 
-def test_oselm_regression(n_samples=400, n_hidden=25, activation_func='tanh', plot=True):
-    x, y = make_regression(n_samples=n_samples, n_targets=1, n_features=10)
-    #data = load_diabetes()
-    #x, y = data['data'], data['target']
-
-    n_batches = n_samples / n_hidden
-
-    n_batch = n_hidden
-
-    oselmr = OSELMRegressor(n_hidden=n_hidden, activation_func=activation_func)
-
-    ypred = []
+    scores, preds = [], []
 
     tic = time.time()
 
     for i in range(n_batches):
         x_batch = x[i*n_batch:(i+1)*n_batch]
         y_batch = y[i*n_batch:(i+1)*n_batch]
-        oselmr.fit(x_batch, y_batch)
-        ypred.extend(oselmr.predict(x_batch))
-        print("Train score for batch %i: %s" % (i+1, str(oselmr.score(x_batch, y_batch))))
+
+        model.fit(x_batch, y_batch)
+        preds.extend(model.predict(x_batch))
+        scores.append(model.score(x_batch, y_batch))
+        print("Train score for batch %i: %s" % (i+1, str(scores[-1])))
+
+    print("Train score - online: %s" % str(np.mean(scores)))
+    print("Train score - offline: %s" % str(model.score(x, y)))
 
     toc = time.time()
 
     print("Total time: %.3f seconds" % (toc-tic))
 
+    return model, preds
+
+
+def make_sequential_predictions(model, x, y):
+    n_samples = len(y)
+
+    # The first batch of data should have the same size as neurons in the model to achieve the 1st phase (boosting)
+    batches_x = [x[:model.n_hidden]] + [[x_i] for x_i in x[model.n_hidden:]]
+    batches_y = [y[:model.n_hidden]] + [[y_i] for y_i in y[model.n_hidden:]]
+
+    print("Testing over %i samples in a online way..." % n_samples)
+
+    preds = []
+
+    tic = time.time()
+
+    for b_x, b_y in zip(batches_x, batches_y):
+
+        model.fit(b_x, b_y)
+        preds.extend(model.predict(b_x))
+
+    print("Train score of total: %s" % str(model.score(x, y)))
+
+    toc = time.time()
+
+    print("Total time: %.3f seconds" % (toc-tic))
+
+    return model, preds
+
+
+def test_oselm_regression_batch(n_samples=400, n_hidden=25, activation_func='tanh', plot=True):
+    x, y = make_regression(n_samples=n_samples, n_targets=1, n_features=10)
+
+    oselmr = OSELMRegressor(n_hidden=n_hidden, activation_func=activation_func)
+
+    oselmr, y_pred = make_batch_predictions(model=oselmr, x=x, y=y, n_batch=n_hidden)
+
     if plot is True:
+        import matplotlib.pyplot as plt
+
         axis_x = range(len(y))
-        plt.plot(axis_x, y, axis_x, ypred)
+        plt.plot(axis_x, y, axis_x, y_pred)
         plt.show()
-
-
-def test_oselm_regression_toy():
-    stdsc = StandardScaler()
-
-    N = 200  # Total data
-    total_batches = 5
-    n_batch = N / total_batches  # batch size
-
-    xtoy, ytoy = make_toy_data(n=N)
-    xtoy, ytoy = stdsc.fit_transform(xtoy), stdsc.fit_transform(ytoy)
-
-    #xtoy_train, xtoy_test, ytoy_train, ytoy_test = train_test_split(xtoy, ytoy, test_size=0.2)
-    #plt.plot(xtoy, ytoy)
-    #plt.show()
-
-    oselmr = OSELMRegressor(n_hidden=20, activation_func='tanh')
-
-    ypred = []
-
-    for i in range(total_batches):
-        xtoy_batch = xtoy[i*n_batch:(i+1)*n_batch]
-        ytoy_batch = ytoy[i*n_batch:(i+1)*n_batch]
-        oselmr.fit(xtoy_batch, ytoy_batch)
-        ypred.extend(oselmr.predict(xtoy_batch))
-        print("Train score for batch %i: %s" % (i+1, str(oselmr.score(xtoy_batch, ytoy_batch))))
-
-    plt.plot(xtoy, ytoy, xtoy, ypred)
-    plt.show()
 
 
 def test_oselm_regression_sequential(n_samples=2000, n_hidden=20, activation_func='tanh', plot=True):
     x, y = make_regression(n_samples=n_samples, n_targets=1, n_features=10)
 
-    start_i = n_hidden+20
-
-    batches_x = [x[:start_i]] + [[x_i] for x_i in x[start_i:]]
-    batches_y = [y[:start_i]] + [[y_i] for y_i in y[start_i:]]
-
     oselmr = OSELMRegressor(n_hidden=n_hidden, activation_func=activation_func)
 
-    ypred = []
-
-    tic = time.time()
-
-    for b_x, b_y in zip(batches_x, batches_y):
-        oselmr.fit(b_x, b_y)
-        ypred.extend(oselmr.predict(b_x))
-
-    print("Train score for total: %s" % str(oselmr.score(x, y)))
-
-    toc = time.time()
-
-    print("Total time: %.3f seconds" % (toc-tic))
+    oselmr, y_pred = make_sequential_predictions(model=oselmr, x=x, y=y)
 
     if plot is True:
+        import matplotlib.pyplot as plt
+
         axis_x = range(len(y))
-        plt.plot(axis_x, y, axis_x, ypred)
+        plt.plot(axis_x, y, axis_x, y_pred)
         plt.show()
 
 
-def test_oselm_classification(n_batches=10, n_classes=8, n_hidden=100, activation_func='sigmoid'):
-    data = load_digits(n_class=n_classes)
-    x, y = data['data'], data['target']
+def test_oselm_classification_batch2(n_batches=10, n_hidden=100, activation_func='sigmoid'):
+    x, y = load_digits(n_class=10, return_X_y=True)
 
     # Shuffle data
     zip_x_y = zip(x, y)
@@ -130,40 +115,77 @@ def test_oselm_classification(n_batches=10, n_classes=8, n_hidden=100, activatio
 
     n_batch = n_samples/n_batches  # batch size
 
-
-    oselmc = OSELMClassifier(n_hidden=n_hidden, activation_func=activation_func)
-    scores = []
+    oselmc = OSELMClassifierSoftmax(n_hidden=n_hidden, activation_func=activation_func)
+    y_pred, scores = [], []
 
     for i in range(n_batches):
         x_batch = x[i*n_batch:(i+1)*n_batch]
         y_batch = y[i*n_batch:(i+1)*n_batch]
         oselmc.fit(x_batch, y_batch)
-        #ypred.extend(oselmc.predict(x_batch))
+        y_pred.extend(oselmc.predict(x_batch))
         score_batch = oselmc.score(x_batch, y_batch)
         scores.append(score_batch)
         print("Train score for batch %i: %s" % (i+1, str(score_batch)))
 
     print("Train score - online: %s" % str(np.mean(scores)))
     print("Train score - offline: %s" % str(oselmc.score(x, y)))
+    print("Confusion matrix: \n %s" % str(confusion_matrix(y[:-1], y_pred)))
 
 
-test_modes = {'regression': lambda: test_oselm_regression_sequential(n_hidden=50, n_samples=1000,
-                                                                     activation_func='tanh', plot=True),
-              'classification': lambda: test_oselm_classification(n_hidden=100, n_batches=4, n_classes=10)
+def test_oselm_classification_batch(n_hidden=100, activation_func='sigmoid'):
+    x, y = load_digits(n_class=10, return_X_y=True)
+
+    # Shuffle data
+    zip_x_y = zip(x, y)
+    random.shuffle(zip_x_y)
+    x, y = [x_y[0] for x_y in zip_x_y], [x_y[1] for x_y in zip_x_y]
+
+    oselmc = OSELMClassifier(n_hidden=n_hidden, activation_func=activation_func)
+
+    oselmc, y_pred = make_batch_predictions(model=oselmc, x=x, y=y, n_batch=n_hidden)
+
+    max_len = min(len(y), len(y_pred))
+
+    print("Confusion matrix: \n %s" % str(confusion_matrix(y[:max_len], y_pred[:max_len])))
+
+
+def test_oselm_classification_sequential(n_hidden=100, activation_func='sigmoid'):
+    x, y = load_digits(n_class=10, return_X_y=True)
+
+    # Shuffle data
+    zip_x_y = zip(x, y)
+    random.shuffle(zip_x_y)
+    x, y = [x_y[0] for x_y in zip_x_y], [x_y[1] for x_y in zip_x_y]
+
+    oselmc = OSELMClassifier(n_hidden=n_hidden, activation_func=activation_func)
+
+    oselmc, y_pred = make_sequential_predictions(model=oselmc, x=x, y=y)
+
+    max_len = min(len(y), len(y_pred))
+
+    print("Confusion matrix: \n %s" % str(confusion_matrix(y[:max_len], y_pred[:max_len])))
+
+
+test_modes = {
+              'classification_batch': lambda: test_oselm_classification_batch(n_hidden=100),
+              'classification_sequential': lambda: test_oselm_classification_sequential(n_hidden=100),
+              'regression_batch': lambda: test_oselm_regression_batch(n_hidden=50, n_samples=1000,
+                                                                      activation_func='tanh', plot=False),
+              'regression_sequential': lambda: test_oselm_regression_sequential(n_hidden=50, n_samples=1000,
+                                                                                activation_func='tanh',
+                                                                                plot=False)
               }
 
 
 if __name__ == '__main__':
-    mode = 'regression'
+    mode = 'regression_batch'
 
     if len(sys.argv) > 1:
 
         argmode = sys.argv[1]
 
-        if argmode == 'classification':
-            mode = 'classification'
-        else:
-            mode = 'regression'
+        if argmode in test_modes:
+            mode = argmode
 
     print("Executing test in mode=%s..." % mode)
 
